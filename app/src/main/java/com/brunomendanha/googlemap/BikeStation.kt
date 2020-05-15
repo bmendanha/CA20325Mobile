e9a0a0c5526efa217a0a3f85318c946215cc6e30
Bruno Mendanha <bmendanha@hotmail.com>
2020-05-15 19:22:48 +0100

    Initial commit

---
+++ app/src/main/java/com/brunomendanha/googlemap/BikeStation.kt
@@ -0,0 +1,4 @@
+package com.brunomendanha.googlemap
+class Position (var lat: Double, var lng: Double )
+class BikeStation(var number: Int, var address: String, var position: Position)
+class Stations(val stations: List<BikeStation>)
